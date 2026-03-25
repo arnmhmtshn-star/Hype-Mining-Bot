03ad8fb2954d9db77750a50201a7e6ef4c406d4d
arnmhmtshn-star <arnmhmtshn@gmail.com>
2026-03-25 23:57:52 +0300

    Change webhook handling to POST and update URL
    Refactor webhook handling to use POST method and simplify webhook URL.

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
-import { webhookCallback } from "grammy";
 import bot from "./bot/index.js";
 
 dotenv.config();
-console.log("TOKEN PREFIX:", process.env.BOT_TOKEN?.slice(0, 8));
-console.log("TOKEN LENGTH:", process.env.BOT_TOKEN?.length);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const webhookPath = `/webhook/${process.env.BOT_TOKEN}`;
-app.use(webhookPath, webhookCallback(bot, "express"));
-
 app.get("/", (req, res) => {
   res.send("Bot çalışıyor! ✅");
+});
+
+app.post("/webhook", async (req, res) => {
+  try {
+    await bot.handleUpdate(req.body);
+    res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
 });
 
 async function main() {
@@ -25,7 +29,7 @@ async function main() {
   });
 
   await bot.api.setWebhook(
-    `${process.env.WEBHOOK_URL}/webhook/${process.env.BOT_TOKEN}`
+    `${process.env.WEBHOOK_URL}/webhook`
   );
   console.log("✅ Webhook ayarlandı");
 }
